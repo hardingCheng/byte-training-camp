d2df6224aa9b7d0a9c985739b91b2b739ea5358a
hardingcheng <hardingcheng@163.com>
2021-09-21 12:33:41 +0800

    feat:webpack代码分割  及其  多入口减小包的大小

--- day_02/webpack/01/webpack.config.js
+++ day_02/webpack/01/webpack.config.js
@@ -1,18 +1,69 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {plugin} = require("./webpack.config");
 module.exports = {
     //入口文件
-    mode:'development',
-    entry: './src/main.js',
+    mode: 'development',
+    ntry: {
+        index: './src/index.js',
+        another: './src/another-module.js',
+
+        // 方式一
+        // 使用 dependOn 把重复的代码可以抽离出去
+        // index: {
+        //   import: "./src/index.js",
+        //   dependOn: "shared",
+        // },
+        // another: {
+        //   import: "./src/another-module.js",
+        //   dependOn: "shared",
+        // },
+        // shared: ["lodash"],
+    },
     output: {
         filename: 'bundle.js',
         //必须是一个绝对路径
-        path:path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clear: true
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    // 方式二
+    // 也可以使用 splitChunks 自动来抽取重复的代码
+    // optimization: {
+    //     splitChunks: {
+    //         chunks: 'all',
+    //     },
+    // },
     module: {
         rules: [{
             test: /\.css$/,
-            use:['style-loader','css-loader']
-        }]
-    }
+            use: ['style-loader', 'css-loader']
+        },// 加载字体和加载图片一样
+            // webpack5 都已经给提供了内置的 loader 来处理
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            //     type: "asset/resource",
+            // },
+            // {
+            //     test: /\.json5$/i,
+            //     type: "json",
+            //     parser: {
+            //         parse: json5.parse,
+            //     },
+            // },
+
+        ]
+    },
+    plugin: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ]
+
 }
